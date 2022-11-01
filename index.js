5caa2aaae3188371bf1aee2865da419eefb3964a
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-10-31 20:36:20 -0500

    commit tree 9f600e5ec83e409b285514a31a65d16440b3efbd

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    let distanceInBlocks = Math.abs(pickUp - 42)
+    return distanceInBlocks
+}
+
+function distanceFromHqInFeet(pickUp) {
+    let distanceInFeet = distanceFromHqInBlocks(pickUp) * 264
+    return distanceInFeet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distanceTravelled = Math.abs(destination - start) * 264
+    return distanceTravelled
+}
+
+function calculatesFarePrice(start, destination) {
+    let fareDistance = Math.abs(destination - start) * 264;
+    let fare;
+    if (fareDistance <= 400) {
+        fare = 0;
+    }
+    else if (fareDistance <= 2000) {
+        fare = (fareDistance - 400) * 0.02;
+    }
+    else if (fareDistance < 2500) {
+        fare = 25;
+    }
+    else {
+        fare = 'cannot travel that far';
+    }
+    return fare
+}
